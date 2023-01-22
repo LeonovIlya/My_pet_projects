953d36baee28ef233daa46063eb2e04def7e5563
LeonovIlya <zlayapisya@gmail.com>
2023-01-22 22:01:42 +0300

    version 0.3a

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-TOKEN = ${{ TOKEN }}
-WEATHER_API = ${{ WEATHER_API }}
-GS_ID = ${{ GS_ID }}
-FILENAME_GS = ${{ FILENAME_GS }}
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+WEATHER_API = os.getenv("WEATHER_API")
+GS_ID = os.getenv("GS_ID")
+FILENAME_GS = os.getenv("FILENAME_GS")
